d456834934d3525c73914c88053866b59cf2b84e
AndersonAslap <anderson.adolfo1998@gmail.ccom>
2023-11-09 14:30:49 -0300

    feat: init project with react and tailwindcss

---
+++ src/pages/Player.tsx
@@ -0,0 +1,20 @@
+export function Player() {
+  return (
+    <div 
+      className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+      <div 
+        className="flex w-{1100px} flex-col gap-6">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-1">
+              <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
+              <span className="text-small text-zinc-400">Módulo "Desvandando o Redux"</span>
+            </div>
+          </div>
+
+          <main>
+
+          </main>
+      </div>
+    </div>
+  )
+}
