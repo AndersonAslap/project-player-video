a8a9ef5dd44e719235d42a424807fc2fdf33feee
AndersonAslap <anderson.adolfo1998@gmail.com>
2023-11-10 00:29:48 -0300

    feat: auto-play

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,11 @@
 import ReactPlayer from "react-player";
 import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { next } from "../store/slices/player";
 
 export function Video() {
+
+  const dispatch = useDispatch()
 
   const video = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
@@ -11,6 +15,10 @@ export function Video() {
     return currentLesson;
   })
 
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="flex-1">
       <div className="w-full bg-zinc-950 aspect-video">
@@ -18,6 +26,8 @@ export function Video() {
           width="100%"
           height="100%"
           controls
+          onEnded={handlePlayNext}
+          playing
           url={`https://www.youtube.com/watch?v=${video.id}`}
         />
       </div>
