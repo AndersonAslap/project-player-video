95897e327954fe9b3047dc58dca1c141b720b837
AndersonAslap <anderson.adolfo1998@gmail.com>
2023-11-12 14:09:01 -0300

    feat: create async thunks

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,5 +1,6 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { useAppSelector } from "..";
+import { api } from "../../lib/axios";
 
 type Lesson = {
   id: string,
@@ -30,15 +31,19 @@ const initialState: PlayerState = {
   currentLessonIndex: 0
 }
 
+export const loadCourse = createAsyncThunk(
+  'player/load',
+  async () => {
+    const response = await api.get('/courses/1');
+    return response.data;
+  }
+)
+
 
 export const playerSlice = createSlice({
     name: 'player',
     initialState,
     reducers: {
-      start: (state, action:PayloadAction<Course>) => {
-        state.course = action.payload;
-      },
-
       play: (state, action: PayloadAction<[number, number]>) => {
         state.currentModuleIndex = action.payload[0];
         state.currentLessonIndex = action.payload[1];
@@ -61,11 +66,16 @@ export const playerSlice = createSlice({
         }
         return state
       },
+    },
+    extraReducers(builder) {
+      builder.addCase(loadCourse.fulfilled, (state, action) => {
+        state.course = action.payload;
+      });
     }
 });
 
 export const player = playerSlice.reducer;
-export const { start, play, next } = playerSlice.actions;
+export const { play, next } = playerSlice.actions;
 
 export const useCurrentLesson = () => {
   return useAppSelector(state => {
